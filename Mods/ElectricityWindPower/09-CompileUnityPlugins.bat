23fded11607ddc5487bc4d7dab884eb3aa0e058e
vic512 <49626238+vic512@users.noreply.github.com>
2022-03-28 08:41:50 -0500

    Added craftable solar mod and removed windmills mod

--- Mods/ElectricityWindPower/09-CompileUnityPlugins.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-call MC7D2D UnityPlugins\ElectricityWindPower.dll ^
-/reference:"%PATH_7D2D_MANAGED%\Assembly-CSharp.dll" ^
-UnityPlugins\*.cs && ^
-echo Successfully compiled UnityPlugins\ElectricityWindPower.dll
-
-pause
